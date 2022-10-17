b7fc9098f234c363bca6fc2a0dd7fbefb05b1d29
reiya-sakamoto <reiya.sakamoto15@gmial.com>
2022-10-17 13:34:08 +0900

    Springboot簡易ログインページ

---
+++ src/main/java/com/example/easylogin/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.easylogin.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.easylogin.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findByUserNameAndPassword(String userName,String password);
+}
